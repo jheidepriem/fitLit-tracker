66c227a9ab81626a18d15cc7a8e71db30e676336
Matt Rowan <matt.rowan121@gmail.com>
2022-12-03 17:14:44 -0500

    Setup tests for user properties

--- test/User-test.js
+++ test/User-test.js
@@ -20,8 +20,36 @@ describe('User', () => {
     it.skip('should be a function', () => {
         expect(User).to.be.a('function');
     });
-    
+
     it.skip('should be an instance of user', () => {
         expect(user).to.be.an.instanceOf(User);
+    });
+
+    it.skip('should have an id', ()=> {
+        expect(user.id).to.equal(1);
+    });
+
+    it.skip('should have a name', () => {
+        expect(user.name).to.equal('Luisa Hane');
+    });
+
+    it.skip('should have an address', () => {
+        expect(user.address).to.equal('15195 Nakia Tunnel, Erdmanport VA 19901-1697');
+    });
+
+    it.skip('should have an email', () => {
+        expect(user.email).to.equal('Diana.Hayes1@hotmail.com');
+    });
+
+    it.skip('should have an stride length', () => {
+        expect(user.strideLength).to.equal(4.3);
+    });
+
+    it.skip('should have an daily step goal', () => {
+        expect(user.dailyStepGoal).to.equal(10000);
+    });
+    
+    it.skip('should have an friends which is an array of nums', () => {
+        expect(user.friends).to.equal([16, 4, 8]);
     });
 });
