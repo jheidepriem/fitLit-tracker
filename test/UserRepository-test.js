f2647b54fc95c7eddb5e9e6662329f775d790edf
Josephine Heidepriem <jheidepriem@gmail.com>
2022-12-03 15:33:31 -0600

    Setup variables to use in beforeEach

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -2,6 +2,45 @@ import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
 
 describe('User Repository', () => {
+  let user1
+  let user2
+  let userRepo1
+  beforeEach(() => {
+    user1 = new User (
+      {
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    }
+    )
+    user2 = (
+      {
+      "id": 2,
+      "name": "Jarvis Considine",
+      "address": "30086 Kathryn Port, Ciceroland NE 07273",
+      "email": "Dimitri.Bechtelar11@gmail.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 5000,
+      "friends": [
+        9,
+        18,
+        24,
+        19
+      ]
+    }
+    )
+    repo1 = new UserRepository([user1, user2])
+  })
+
+
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
