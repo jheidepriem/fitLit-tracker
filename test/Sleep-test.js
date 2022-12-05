a3eaa4af42f91e13ee6d4cd13194ddcedc77c66e
Angela Wirth <angiewirth92@gmail.com>
2022-12-05 09:11:43 -0500

    Write tests for new instance, properties and method

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -8,6 +8,8 @@ describe("Sleep", () => {
   let sleepEntry4;
   let sleepEntry5;
   let sleepEntry6;
+  let data;
+  let sleep1;
 
   beforeEach(() => {
     sleepEntry1 = {
@@ -15,36 +17,58 @@ describe("Sleep", () => {
       date: "2019/06/15",
       hoursSlept: 6.1,
       sleepQuality: 2.2
-      }
+      };
     sleepEntry2 = {
       userID: 2,
       date: "2019/06/15",
       hoursSlept: 7,
       sleepQuality: 4.7
-      }
+      };
     sleepEntry3 = {
       userID: 1,
       date: "2019/06/16",
       hoursSlept: 4.1,
       sleepQuality: 3.8
-      }
+      };
     sleepEntry4 = {
       userID: 1,
       date: "2019/06/17",
       hoursSlept: 8,
       sleepQuality: 2.6
-      }
+      };
     sleepEntry5 = {
       userID: 1,
       date: "2019/06/18",
       hoursSlept: 10.4,
       sleepQuality: 3.1
-      }
+      };
     sleepEntry6 = {
       userID: 1,
       date: "2019/06/19",
       hoursSlept: 10.7,
       sleepQuality: 1.2
-      }
-  })
+      };
+    data = [sleepEntry1, sleepEntry2, sleepEntry3, sleepEntry4, sleepEntry5, sleepEntry6]
+    sleep1 = new Sleep(1, data)
+  });
+
+  it('should be a function', () => {
+    expect(Sleep).to.be.a("function")
+  });
+
+  it("should be an instance of sleep", () => {
+    expect(sleep1).to.be.an.instanceOf(Sleep)
+  });
+
+  it("should have a user id", () => {
+    expect(sleep1.id).to.equal(1)
+  });
+
+  it("should have an array of sleep data", () => {
+    expect(sleep1.sleepData).to.deep.equal(data)
+  });
+
+  it("should have a method that filters the sleep data by user id", () => {
+    expect(sleep1.filterSleepData()).to.deep.equal([sleepEntry1, sleepEntry3, sleepEntry4, sleepEntry5, sleepEntry6])
+  });
 })
