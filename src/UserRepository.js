dba0dac603c35e1d1cc20ea4670fbe0e657d6895
Josephine Heidepriem <jheidepriem@gmail.com>
2022-12-05 19:04:23 -0600

    Create findAverageStepGoal function

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -5,7 +5,16 @@ class UserRepository {
 
   retrieveUserData(id){
     let singleUserData = this.userData.find((user) => user.id === id)
-    return singleUse
+    return singleUserData
+  }
+
+  findAverageStepGoal(){
+    const sum = this.userData.reduce((total, user) => {
+      total = total += user.dailyStepGoal 
+      return total 
+    }, 0)
+    let average = sum / this.userData.length
+    return average
   }
 }
 export default UserRepository;
