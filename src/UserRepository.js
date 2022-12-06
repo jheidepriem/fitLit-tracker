56eac2c3d71044f247bbcf6cc0bb3cb690f18b03
Josephine Heidepriem <jheidepriem@gmail.com>
2022-12-05 18:50:24 -0600

    Add retrieveUserData class

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,8 +1,11 @@
 class UserRepository {
-  constructor(){
-    
+  constructor(data){
+    this.userData = data
   }
 
+  retrieveUserData(id){
+    let singleUserData = this.userData.find((user) => user.id === id)
+    return singleUse
+  }
 }
-
 export default UserRepository;
