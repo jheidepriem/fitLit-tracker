0366c7b33e94b7253197285434b6093ef93bca24
Matt Rowan <matt.rowan121@gmail.com>
2022-12-08 10:52:14 -0500

    Remove console log from line 5 of apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,7 +2,7 @@
 const fetchApiUrl = (path) => {
   return fetch(`https://fitlit-api.herokuapp.com/api/v1/${path}`)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => data)
   .catch(error => console.log(`${path} error`))
 }
 
