330ea1cfa9523ac2e723fb2ec17c542b5a57e15c
Matt Rowan <matt.rowan121@gmail.com>
2022-12-09 12:18:59 -0500

    Adjust indentations and update tests on lines 106, 110, and 114

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -19,49 +19,49 @@ describe("Sleep", () => {
       date: "2019/06/15",
       hoursSlept: 6.1,
       sleepQuality: 2.2
-      };
+    };
     sleepEntry2 = {
       userID: 2,
       date: "2019/06/15",
       hoursSlept: 7,
       sleepQuality: 4.7
-      };
+    };
     sleepEntry3 = {
       userID: 1,
       date: "2019/06/16",
       hoursSlept: 4.1,
       sleepQuality: 3.8
-      };
+    };
     sleepEntry4 = {
       userID: 1,
       date: "2019/06/17",
       hoursSlept: 8,
       sleepQuality: 2.6
-      };
+    };
     sleepEntry5 = {
       userID: 1,
       date: "2019/06/18",
       hoursSlept: 10.4,
       sleepQuality: 3.1
-      };
+    };
     sleepEntry6 = {
       userID: 1,
       date: "2019/06/19",
       hoursSlept: 10.7,
       sleepQuality: 1.2
-      };
+    };
     sleepEntry7 = {
       userID: 1,
       date: "2019/06/20",
       hoursSlept: 8,
       sleepQuality: 3.4
-      };
+    };
     sleepEntry8 = {
       userID: 1,
       date: "2019/06/21",
       hoursSlept: 10.1,
       sleepQuality: 1.8
-      }
+    }
     
     data = [sleepEntry1, sleepEntry2, sleepEntry3, sleepEntry4, sleepEntry5, sleepEntry6, sleepEntry7, sleepEntry8]
     sleep1 = new Sleep(1, data)
@@ -104,16 +104,14 @@ describe("Sleep", () => {
   });
 
   it('should have a method that returns the total hours slept each day over the course of a week', () => {
-    expect(sleep1.totalWeeklyHours("2019/06/21")).to.equal()
+    expect(sleep1.totalWeeklyHours("2019/06/21")).to.deep.equal([ 6.1, 4.1, 8, 10.4, 10.7, 8 ])
   });
 
-  it.skip('should have a method that returns the total sleep quality each day over the course of a week', () => {
-    expect(sleep1.totalWeeklyQuality("2019/06/21")).to.equal()
+  it('should have a method that returns the total sleep quality each day over the course of a week', () => {
+    expect(sleep1.totalWeeklyQuality("2019/06/21")).to.deep.equal([ 2.2, 3.8, 2.6, 3.1, 1.2, 3.4 ])
   });
 
   it('should have a method that returns the average sleep quality across all users', () => {
-    expect(sleep1.averageAllUsersQuality()).to.equal()
+    expect(sleep1.averageAllUsersQuality()).to.equal(2.85)
   });
-
-
 })
