63adcd9833b4f9c77aa332d0af9d4f27171b136b
Angela Wirth <angiewirth92@gmail.com>
2022-12-11 11:19:35 -0500

    Remove boiler plate comments

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,4 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS file
 import "./css/styles.css";
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import "./images/turing-logo.png";
-
-// An example of how you tell webpack to use a JS file
-
 import UserRepository from "./UserRepository";
 import Hydration from "../src/Hydration";
 import Sleep from "../src/Sleep";
