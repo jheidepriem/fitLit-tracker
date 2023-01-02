500cdb29d3f7d0bdfd1d14d4a66dd55e062b8b04
Reid Poole <poole.reid@gmail.com>
2023-01-02 16:19:17 -0600

    Refactor totalWeekly method in sleep test

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -100,16 +100,10 @@ describe("Sleep", () => {
     expect(sleep1.giveDaily("2019/06/15", "hoursSlept")).to.equal(6.1)
   });
 
-  // it('should have a method that returns the sleep quality for a single date', () => {
-  //   expect(sleep1.giveDailyQuality("2019/06/15")).to.equal(2.2)
-  // });
 
-  it('should have a method that returns the total hours slept each day over the course of a week', () => {
-    expect(sleep1.totalWeeklyHours("2019/06/21")).to.deep.equal([ 6.1, 4.1, 8, 10.4, 10.7, 8 ])
-  });
-
-  it('should have a method that returns the total sleep quality each day over the course of a week', () => {
-    expect(sleep1.totalWeeklyQuality("2019/06/21")).to.deep.equal([ 2.2, 3.8, 2.6, 3.1, 1.2, 3.4 ])
+  it('should have a method that returns the specified measurement each day over the course of a week', () => {
+    expect(sleep1.totalWeekly("2019/06/21", "hoursSlept")).to.deep.equal([ 6.1, 4.1, 8, 10.4, 10.7, 8 ])
+    expect(sleep1.totalWeekly("2019/06/21", "sleepQuality")).to.deep.equal([ 2.2, 3.8, 2.6, 3.1, 1.2, 3.4 ])
   });
 
   it('should have a method that returns the average sleep quality across all users', () => {
