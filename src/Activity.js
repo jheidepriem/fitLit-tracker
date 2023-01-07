cbcfb358b129d9ebaf314ea9f5160b8c11653756
Angela Wirth <angiewirth92@gmail.com>
2023-01-07 13:02:02 -0500

    Remove console logs

--- src/Activity.js
+++ src/Activity.js
@@ -91,18 +91,14 @@ class Activity {
         const latestDate = this.allData.slice(-1)[0].date
         const filteredData = this.allData.filter(entry => entry.date === date)
         const noDate =  this.allData.filter(entry => entry.date === latestDate)
-        console.log('noDate:', noDate)
-
         if(arguments.length === 2) {
             const avgData = filteredData.reduce((acc, entry) => {
                 acc += entry[measure]
                 return acc
             }, 0)/filteredData.length
-            console.log('MEASURE:', measure)
             return Number(avgData.toFixed(1))
         } else {
             const avgNoDateData = noDate.reduce((acc, entry) => {
-                // console.log('measure:', measure)
                 acc += entry[measure]
                 return acc
             }, 0)
