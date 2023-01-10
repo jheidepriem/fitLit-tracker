6878ffd251898212b6ee40100478d4cf9837da9b
Josephine Heidepriem <jheidepriem@gmail.com>
2023-01-09 20:03:13 -0600

    Refactor spacing

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,57 +1,59 @@
 class Sleep {
   constructor(userID, data) {
-    this.id = userID
-    this.allData = data
-    this.sleepHistory = data.filter((sleepEntry) => sleepEntry.userID === this.id);
+    this.id = userID;
+    this.allData = data;
+    this.sleepHistory = data.filter(
+      (sleepEntry) => sleepEntry.userID === this.id
+    );
   }
 
   calcDailyHrsAvg() {
     const sum = this.sleepHistory.reduce((total, day) => {
-      return total += day.hoursSlept
-    },0)
-    const avg = sum/this.sleepHistory.length
-    return Number(avg.toFixed(1))
-  };
+      return (total += day.hoursSlept);
+    }, 0);
+    const avg = sum / this.sleepHistory.length;
+    return Number(avg.toFixed(1));
+  }
 
   calcDailyQualityAvg() {
     const sum = this.sleepHistory.reduce((total, day) => {
-      return total += day.sleepQuality
-    },0)
-    const avg = sum/this.sleepHistory.length
-    return Number(avg.toFixed(1))
-  };
+      return (total += day.sleepQuality);
+    }, 0);
+    const avg = sum / this.sleepHistory.length;
+    return Number(avg.toFixed(1));
+  }
 
   giveDaily(date, measure) {
-    const pickedDay = this.sleepHistory.find(day => day.date === date)
-    if(!pickedDay) {
-      return undefined
+    const pickedDay = this.sleepHistory.find((day) => day.date === date);
+    if (!pickedDay) {
+      return undefined;
     } else {
-      return pickedDay[measure]
+      return pickedDay[measure];
     }
-  };
+  }
 
   totalWeekly(date, measure) {
-    const pickedDay = this.sleepHistory.findIndex(obj => {
-      const dateIndex = obj.date === date 
-      return Number(dateIndex)
-    })
+    const pickedDay = this.sleepHistory.findIndex((obj) => {
+      const dateIndex = obj.date === date;
+      return Number(dateIndex);
+    });
     let selectedWeek;
-    if(pickedDay <= 6) {
-      selectedWeek = this.sleepHistory.slice(0, 7)
+    if (pickedDay <= 6) {
+      selectedWeek = this.sleepHistory.slice(0, 7);
     } else {
-      selectedWeek = this.sleepHistory.slice((pickedDay - 6), pickedDay + 1)
+      selectedWeek = this.sleepHistory.slice(pickedDay - 6, pickedDay + 1);
     }
-    const weeklyMeasure = selectedWeek.map(day => day[measure])
-     return weeklyMeasure
-  };
+    const weeklyMeasure = selectedWeek.map((day) => day[measure]);
+    return weeklyMeasure;
+  }
 
   averageAllUsersQuality() {
     const sum = this.allData.reduce((total, day) => {
-      return total += day.sleepQuality
-    },0)
-    const avg = sum/this.allData.length
-    return Number(avg.toFixed(1))
-  };
+      return (total += day.sleepQuality);
+    }, 0);
+    const avg = sum / this.allData.length;
+    return Number(avg.toFixed(1));
+  }
 }
 
 module.exports = Sleep;
